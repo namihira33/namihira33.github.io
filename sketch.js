df4c162fbca4492f71859791b8824819df163084
namihira33 <syuntakosu1011@gmail.com>
2021-12-20 00:13:32 +0900

    Voiceanalysis

--- sketch.js
+++ sketch.js
@@ -308,7 +308,7 @@ function draw(){
         textSize(16);
         text(30*str_size/recframe,100,300);
         text(recframe,100,400);
-        text(result_str,824,47,284,600);
+        text(result_str,824,52,228,560);
         text(43*id,100,100);
         text(max,100,200);
 
